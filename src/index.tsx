7d16e927d53b12a6075bcd390363567f615ec126
Ville Turku <ville.turku99@gmail.com>
2021-05-20 18:29:17 +0300

    Init create-react-app, cleaned unnecessary

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>
+  <div>
+    lol
+  </div>
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
