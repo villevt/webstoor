153cca20a82e1cfbb1578faad86811bce108fd37
Ville Turku <ville.turku99@gmail.com>
2021-05-21 13:38:20 +0300

    Basic theming for colors

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Logo} from "./components/logo";
 
-import styles from "./index.module.css";
 import Container from 'react-bootstrap/Container';
-import Jumbotron from "react-bootstrap/Jumbotron";
+
+import './custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="overflow-hidden min-vh-100">
+    <div className="bg-primary overflow-hidden min-vh-100">
       <Container fluid>
         <Logo className="d-block mx-auto my-5 w-25"/>
-        <h1 className="text-center display-4">Stoorming with the best deals! </h1>
+        <h1 className="text-center">Stoorming with the best deals!</h1>
       </Container>
     </div>
   </React.StrictMode>,
